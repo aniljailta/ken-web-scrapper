004cbc77b24d9fa4caeb1610cb73a768d51a34f7
anil <aniljailta@gmail.com>
2025-02-05 15:11:10 +0530

    prefix changes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('scraper/api');
+  app.setGlobalPrefix('scraper-api');
   app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
