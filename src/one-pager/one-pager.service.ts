efc2a4b9aac257735f667bc4b3832585cf75c237
aniljailta <aniljailta@gmail.com>
2025-08-29 11:14:50 +0530

    feat:fonts self hosted

--- src/one-pager/one-pager.service.ts
+++ src/one-pager/one-pager.service.ts
@@ -890,7 +890,7 @@ export class OnePagerService {
 
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0' });
-    await page.evaluateHandle('document.fonts.ready'); // ensure fonts are loaded
+    await page.evaluateHandle('document.fonts.ready');
 
     // Create PDF buffer
     const pdfBuffer = await page.pdf({
